95ee63287d06f8ba4b903332685a4a245c72b911
Anas Shekhamis <anas.shekhamis@gmail.com>
2015-09-16 11:28:40 +0300

    Alter description

--- modules/Database.php
+++ modules/Database.php
@@ -2,18 +2,11 @@
 /**
  * Basically CRUD.
  *
- * Basically CRUD is a class that helps creating basic
- * CRUD system/application, and making it maintainable
- * and secure against SQL injection.
- * To start using it, create an object passing the path
- * to DB.ini file, and you're done. Now you can execute
- * SQL queries.
+ * Basically CRUD is a simple class that create a Database Abstraction Layer(DBAL) helping creating basic CRUD systems/applications, making them maintainable and secure against SQL injection.
+ * To start using it, create an object passing the path to DB.ini file, and you're done. Now you can execute SQL queries.
  * However, I created it with these two concepts in mind:
- * DI (Dependency Injection) and Law of Demeter (a.k.a
- * tell, don't ask.) So make sure to use it this way:
- * In your class, declare a dependency of this class in
- * the constructor (or using set method...etc) and start
- * using methods of this class in your class.
+ * DI (Dependency Injection) and Law of Demeter (a.k.a tell, don't ask.)
+ * So make sure to use it this way: In your class, declare a dependency of this class in the constructor (or using set method...etc) and start using methods of this class in your class.
  * For more information checkout the example.
  *
  * @author Anas Shekhamis <anas.shekhamis@gmail.com>
